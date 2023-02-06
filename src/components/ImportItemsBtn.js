001ea6c4d831223732bf4b62ec0321d1df2fb865
Brian Waweru Muciri <bwwaweru18@gmail.com>
2023-02-06 10:20:00 +0300

    Updates.

--- src/components/ImportItemsBtn.js
+++ src/components/ImportItemsBtn.js
@@ -1,4 +1,4 @@
-import Button from "@material-ui/core/Button";
+import Button from "@mui/material/Button";
 import React, { useState } from "react";
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 import { readXlsxFile } from "../assets/PrintingHelper";
--- src/components/InfoDisplayPaper.js
+++ src/components/InfoDisplayPaper.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 import { commonStyles } from "../components/commonStyles";
 
 let InfoDisplayPaper = (props) => {

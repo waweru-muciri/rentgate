a80622da03c7ba044e9c232a68bcb9511d54ed36
Brian Waweru Muciri <bwwaweru18@gmail.com>
2023-03-02 12:15:00 +0300

    Updates.

--- src/components/meterReadings/MeterReadingInputForm.js
+++ src/components/meterReadings/MeterReadingInputForm.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Grid from "@material-ui/core/Grid";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import MenuItem from "@material-ui/core/MenuItem";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import MenuItem from "@mui/material/MenuItem";
 import CustomSnackbar from '../CustomSnackbar'
 import { Formik } from "formik";
 import { commonStyles } from "../commonStyles";
